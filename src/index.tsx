47dddeaa328faf1602d425f44fe7a1068b64f454
Alexandra Egorova <egorova.aleksandra@gmail.com>
2023-04-02 19:44:55 +0200

    bug fixing again

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Router basename="online-shop-project">
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>
 );
