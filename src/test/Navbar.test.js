299d9fe81f1fc6a89cd92e2d27ddf42ade67b311
Alexandra Egorova <egorova.aleksandra@gmail.com>
2023-04-07 11:39:40 +0200

    added some tests

--- src/test/Navbar.test.js
+++ src/test/Navbar.test.js
@@ -1,15 +1,15 @@
 import { render } from "@testing-library/react";
 import Navbar from "../components/Navbar/index";
-
-// interface Props { 
-//   selectedProductQuantity: number;
-//   totalPrice: number;
-// }
+import { HashRouter } from "react-router-dom";
 
 
 describe(Navbar, ()=>{
   it('Navbar displays correct total price', ()=>{
-    const {getByTestId} = render(<Navbar selectedProductQuantity={2} totalPrice={40}/>);
+    const {getByTestId} = render(
+      <HashRouter>
+        <Navbar selectedProductQuantity={2} totalPrice={40}/>
+      </HashRouter>
+    );
     const countValue = Number(getByTestId("totalPrice").textContent);
     expect(countValue).toEqual(40);
   });
--- src/test/ShoppingCart.test.js
+++ src/test/ShoppingCart.test.js
@@ -1,20 +1,48 @@
-import { render } from "@testing-library/react";
+import { getAllByTestId, render } from "@testing-library/react";
 import ShoppingCart from "../components/ShoppingCart";
+import { HashRouter } from "react-router-dom";
 
-
-// interface Props { 
-//   selectedProductQuantity: number;
-//   totalPrice: number;
-// }
-
+const products = [
+  {
+    id: 1,
+    name: "AOS средство для мытья посуды Crystal",
+    url: "./assets/productItem/product1.png",
+    amount: 1,
+    size: 450,
+    sizeType: "мл",
+    barcode: "4604049097548",
+    producer: "Нэфис",
+    brand: "AOS",
+    description: "AOS средство для мытья посуды Crystal",
+    price: 48.76,
+    caretype: ["Бытовая химия"],
+    quantity: 2
+  },
+  {
+    id: 2,
+    name: "ARIEL Автомат Гель СМС жидкое в растворимых капсулах Liquid Capsules Горный родник",
+    url: "./assets/productItem/product2.png",
+    amount: 15,
+    size: 28.8,
+    sizeType: "г",
+    barcode: "4604049097548",
+    producer: "Нэфис",
+    brand: "AOS",
+    description: "Автомат Гель СМС жидкое в растворимых капсулах Liquid Capsules Горный родник",
+    price: 20.00,
+    caretype: ["Бытовая химия"],
+    quantity: 3
+  }
+];
 
 describe(ShoppingCart, ()=>{
   it('ShoppingCart displays correct total price', ()=>{
-    const {getByTestId} = render(<ShoppingCart/>);
-    
-    const quantity = Number(getAllByTestId("quantity").textContent);
-    const price = Number(getAllByTestId("price").textContent);
+    const {getByTestId, getAllByTestId} = render(
+        <ShoppingCart products={products}/>
+    );
+    const quantity = Number(getAllByTestId("quantity")[0].textContent);
+    const price = Number(getAllByTestId("price")[0].textContent);
     const total = Number(getByTestId("totalPrice").textContent);
-    expect(total).toEqual(price*quantity);
+    expect(total).toEqual(price * quantity);
   });
 });
